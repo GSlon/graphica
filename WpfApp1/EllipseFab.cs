07bbccd79772193ba3b862c5c37eae0e81c9b410
staspro <stazzzonpro@gmail.com>
2019-09-23 01:12:34 +0300

    first

---
+++ WpfApp1/EllipseFab.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class EllipseFab
+    {
+        private Ellipse elps;
+        private BitmapCacheBrush br;
+        private TextBlock text;
+
+        public EllipseFab(double x1, double y1, double x2, double y2, string txt = "")
+        {
+            text = new TextBlock
+            {
+                Text = txt,
+                Background = Brushes.Red,
+                Width = 40,
+                Height = 40,
+                TextAlignment = System.Windows.TextAlignment.Center,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+
+            br = new BitmapCacheBrush(text);
+
+            elps = new Ellipse
+            {
+                Width = 50,
+                Height = 50,
+                Opacity = 1,
+                Stroke = Brushes.Black,
+                Margin = new System.Windows.Thickness(x1, y1, x2, y2)
+            };
+
+            elps.Fill = br;
+        }
+
+        //EllipseFab(string text, Int32 x, Int32 y, Brushes brush)  //на случай закраски другим цветом
+        //{
+
+        //}
+
+        public Ellipse GetEllipse()
+        {
+            return elps;
+        }
+    }
+
+}
---
+++ WpfApp1/Graph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    struct Edge
+    {
+        public string Name;
+        public int X, Y;
+    }
+
+    struct Vertex
+    {
+        public int Weight;
+    }
+
+    class Graph
+    {
+        // словарь {Edge: <Edge, Vertex>, <...>}    // сразу лежит связь между вершинами и ребрами
+
+    }
+}
